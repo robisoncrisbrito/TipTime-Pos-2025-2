cc2355840ac0aae5fcd3c6815f960861d0b57b91
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2025-11-22 10:26:03 -0300

    Código com binding e com o tratamento do clique do botão

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,11 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
 }
 
 dependencies {
@@ -42,6 +47,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
